736fcf1cb8ee9424f1c26ed7503533cb1151c209
Ashera Silva <asherasilva100@gmail.com>
2022-06-27 22:37:12 +0530

    Fix scripts to work with APIM 4.0.0

--- distribution/scripts/apim/create-api.sh
+++ distribution/scripts/apim/create-api.sh
@@ -251,192 +251,272 @@ echo -ne "\n"
 # Create GraphQL API
 api_create_request() {
     cat <<EOF
-{ 
-   "name":"$1API",
-   "description":"$2",
-   "context":"/$1",
-   "version":"1.0.0",
-   "provider":"admin",
-   "policies":[ 
-      "Unlimited"
-   ],
-   "endpointConfig":{ 
-      "endpoint_type":"${backend_endpoint_type}",
-      "sandbox_endpoints":{ 
-         "url":"${backend_endpoint_url}"
-      },
-      "production_endpoints":{ 
-         "url":"${backend_endpoint_url}"
-      }
-   },
-   "operations":[
-      {
-         "id":"0",
-         "target":"hero",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"1",
-         "target":"reviews",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],"usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"2",
-         "target":"search",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"3",
-         "target":"character",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"4",
-         "target":"droid",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],"usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"5",
-         "target":"human",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"6",
-         "target":"allHumans",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"7",
-         "target":"allDroids",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"8",
-         "target":"allCharacters",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"9",
-         "target":"starship",
-         "verb":"QUERY",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"10",
-         "target":"createReview",
-         "verb":"MUTATION",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      },
-      {
-         "id":"11",
-         "target":"reviewAdded",
-         "verb":"SUBSCRIPTION",
-         "authType":"Any",
-         "throttlingPolicy":null,
-         "scopes":[],
-         "usedProductIds":[],
-         "amznResourceName":null,
-         "amznResourceTimeout":null,
-         "payloadSchema":null,
-         "uriMapping":null,
-         "operationPolicies":null
-      }
-   ]
+{
+    "name": "$1API",
+    "description": "$2",
+    "context": "/$1",
+    "version": "1.0.0",
+    "provider": "admin",
+    "lifeCycleStatus": "CREATED",
+    "wsdlInfo": null,
+    "wsdlUrl": null,
+    "responseCachingEnabled": false,
+    "cacheTimeout": 300,
+    "hasThumbnail": false,
+    "isDefaultVersion": false,
+    "isRevision": false,
+    "revisionedApiId": null,
+    "revisionId": 0,
+    "enableSchemaValidation": false,
+    "type": "GRAPHQL",
+    "transport": [
+        "http",
+        "https"
+    ],
+    "tags": [],
+    "policies": [
+        "Unlimited"
+    ],
+    "apiThrottlingPolicy": null,
+    "authorizationHeader": "Authorization",
+    "securityScheme": [
+        "oauth2",
+        "oauth_basic_auth_api_key_mandatory"
+    ],
+    "maxTps": null,
+    "visibility": "PUBLIC",
+    "visibleRoles": [
+        ""
+    ],
+    "visibleTenants": [],
+    "mediationPolicies": [],
+    "subscriptionAvailability": "CURRENT_TENANT",
+    "subscriptionAvailableTenants": [
+        ""
+    ],
+    "additionalProperties": [],
+    "monetization": null,
+    "accessControl": "NONE",
+    "accessControlRoles": [],
+    "businessInformation": {
+        "businessOwner": null,
+        "businessOwnerEmail": null,
+        "technicalOwner": null,
+        "technicalOwnerEmail": null
+    },
+    "corsConfiguration": {
+        "corsConfigurationEnabled": false,
+        "accessControlAllowOrigins": [
+            "*"
+        ],
+        "accessControlAllowCredentials": false,
+        "accessControlAllowHeaders": [
+            "authorization",
+            "Access-Control-Allow-Origin",
+            "Content-Type",
+            "SOAPAction",
+            "apikey",
+            "Internal-Key"
+        ],
+        "accessControlAllowMethods": [
+            "GET",
+            "PUT",
+            "POST",
+            "DELETE",
+            "PATCH",
+            "OPTIONS"
+        ]
+    },
+    "websubSubscriptionConfiguration": {
+        "enable": false,
+        "secret": "",
+        "signingAlgorithm": "SHA1",
+        "signatureHeader": "x-hub-signature"
+    },
+    "workflowStatus": null,
+    "createdTime": "2022-06-26 20:49:14.814",
+    "lastUpdatedTime": null,
+    "endpointConfig": {
+        "endpoint_type": "${backend_endpoint_type}",
+        "sandbox_endpoints": {
+            "url": "${backend_endpoint_url}"
+        },
+        "production_endpoints": {
+            "url": "${backend_endpoint_url}"
+        }
+    },
+    "endpointImplementationType": "ENDPOINT",
+    "scopes": [],
+    "operations": [
+        {
+            "id": "",
+            "target": "hero",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "reviews",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "search",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "character",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "droid",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "human",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "allHumans",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "allDroids",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "allCharacters",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "starship",
+            "verb": "QUERY",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "createReview",
+            "verb": "MUTATION",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        },
+        {
+            "id": "",
+            "target": "reviewAdded",
+            "verb": "SUBSCRIPTION",
+            "authType": "Application & Application User",
+            "throttlingPolicy": null,
+            "scopes": [],
+            "usedProductIds": [],
+            "amznResourceName": null,
+            "amznResourceTimeout": null,
+            "payloadSchema": null,
+            "uriMapping": null
+        }
+    ],
+    "threatProtectionPolicies": null,
+    "categories": [],
+    "keyManagers": [
+        "all"
+    ],
+    "serviceInfo": null,
+    "advertiseInfo": {
+        "advertised": false,
+        "originalDevPortalUrl": null,
+        "apiOwner": "admin",
+        "vendor": "WSO2"
+    }
 }
 EOF
 }
@@ -456,7 +536,7 @@ create_api() {
     local api_desc="$2"
     echo "Creating $api_name API..."
     # Check whether API exists
-    local existing_api_id=$($curl_command -H "Authorization: Bearer $view_access_token" ${base_https_url}/api/am/publisher/v3/apis?query=name:$api_name\$ | jq -r '.list[0] | .id')
+    local existing_api_id=$($curl_command -H "Authorization: Bearer $view_access_token" ${base_https_url}/api/am/publisher/v2/apis?query=name:$api_name\$ | jq -r '.list[0] | .id')
     if [ ! -z $existing_api_id ] && [ ! $existing_api_id = "null" ]; then
         echo "$api_name API already exists with ID $existing_api_id"
         echo -ne "\n"
@@ -480,7 +560,7 @@ create_api() {
                 echo -ne "\n"
             fi
 
-            local delete_api_status=$($curl_command -w "%{http_code}" -o /dev/null -H "Authorization: Bearer $create_access_token" -X DELETE "${base_https_url}/api/am/publisher/v3/apis/$existing_api_id")
+            local delete_api_status=$($curl_command -w "%{http_code}" -o /dev/null -H "Authorization: Bearer $create_access_token" -X DELETE "${base_https_url}/api/am/publisher/v2/apis/$existing_api_id")
             if [ $delete_api_status -eq 200 ]; then
                 echo "$api_name API deleted!"
                 echo -ne "\n"
@@ -493,7 +573,7 @@ create_api() {
             return
         fi
     fi
-    local api_id=$($curl_command -H "Authorization: Bearer $create_access_token" -F file=@$script_dir/schema/starwars-schema.graphql -F additionalProperties="$(api_create_request $api_name $api_desc)" ${base_https_url}/api/am/publisher/v3/apis/import-graphql-schema | jq -r '.id')
+    local api_id=$($curl_command -H "Authorization: Bearer $create_access_token" -F file=@$script_dir/schema/starwars-schema.graphql -F additionalProperties="$(api_create_request $api_name $api_desc)" ${base_https_url}/api/am/publisher/v2/apis/import-graphql-schema | jq -r '.id')
     if [ ! -z $api_id ] && [ ! $api_id = "null" ]; then
         echo "Created $api_name API with ID $api_id"
         echo -ne "\n"
@@ -503,11 +583,11 @@ create_api() {
         return
     fi
 
-    local rev_id=$($curl_command -H "Authorization: Bearer $create_access_token" -H "Content-Type: application/json" -X POST -d '{"description": "first revision"}' ${base_https_url}/api/am/publisher/v3/apis/${api_id}/revisions | jq -r '.id')
-    local revisionUuid=$($curl_command -H "Authorization: Bearer $create_access_token" -H "Content-Type: application/json" -X POST -d '[{"name": "Default", "vhost": "localhost" ,"displayOnDevportal": true}]' ${base_https_url}/api/am/publisher/v3/apis/${api_id}/deploy-revision?revisionId=${rev_id} | jq -r '.[0] | .revisionUuid')
+    local rev_id=$($curl_command -H "Authorization: Bearer $create_access_token" -H "Content-Type: application/json" -X POST -d '{"description": "first revision"}' ${base_https_url}/api/am/publisher/v2/apis/${api_id}/revisions | jq -r '.id')
+    local revisionUuid=$($curl_command -H "Authorization: Bearer $create_access_token" -H "Content-Type: application/json" -X POST -d '[{"name": "Default", "vhost": "localhost" ,"displayOnDevportal": true}]' ${base_https_url}/api/am/publisher/v2/apis/${api_id}/deploy-revision?revisionId=${rev_id} | jq -r '.[0] | .revisionUuid')
 
     echo "Publishing $api_name API"
-    local publish_api_status=$($curl_command -w "%{http_code}" -o /dev/null -H "Authorization: Bearer $publish_access_token" -X POST "${base_https_url}/api/am/publisher/v3/apis/change-lifecycle?action=Publish&apiId=${api_id}")
+    local publish_api_status=$($curl_command -w "%{http_code}" -o /dev/null -H "Authorization: Bearer $publish_access_token" -X POST "${base_https_url}/api/am/publisher/v2/apis/change-lifecycle?action=Publish&apiId=${api_id}")
     if [ $publish_api_status -eq 200 ]; then
         echo "$api_name API Published!"
         echo -ne "\n"
